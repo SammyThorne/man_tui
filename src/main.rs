85315ebf9dcf5d3e9451bd64504230108baea2a3
Sammy <sammy@dev-crypt.com>
2026-06-13 09:54:53 -0700

    basic library working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use sysinfo::{System};
+
 fn main() {
-    println!("Hello, world!");
+    let mut sys = System::new_all();
+    
+    for (pid,process) in sys.processes() {
+
+        println!("{} {:?} {}KB, {}%", pid, process.name(),process.memory()/1024, process.cpu_usage());
+
+    }
+
+    println!("All done!");
+
 }
